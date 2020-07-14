9b9cd7847014b7dd6807bcb253b8f5b79a3d48de
Mark Plutowski <mark@flipboard.com>
2020-07-13 18:35:03 -0700

    sets SITEURL,  sets DELETE_OUTPUT_DIRECTORY = False

--- publishconf.py
+++ publishconf.py
@@ -11,13 +11,13 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = ''
+SITEURL = 'https://salsahandbook.github.io/'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
-DELETE_OUTPUT_DIRECTORY = True
+DELETE_OUTPUT_DIRECTORY = False
 
 # Following items are often useful when publishing
 
